02afc545c1adc77079bc690493bd766fdf1b87e0
Glitch (glitch-hello-website) <none>
2024-10-23 09:42:16 +0000

    💽🙎 Checkpoint ./script.js:37674724/613

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ if (btn) { // Detect clicks on the button
 }
 
 // RANDOMISE Q
-
 class Question {
   constructor(t, c, a, k) {
     this.theme = t;
@@ -20,11 +19,15 @@ class Question {
   }
 }
 
-const helloWorld = new Question("Getting started", "QNUUXIEX UM", "HELLO WORLD", 9)
+const helloWorld = new Question("Getting started...", "QNUUXIEX UM", "HELLO WORLD", 9)
+const goodLuck = new Question("Good luck!", "RGUQ PQPAUW", "BREAK A LEG", 16)
+const catTongue = new Question("I'm speechless...", "ECVBIQVB QWTBVQPIWG", "CAT GOT YOUR TONGUE", 2)
+const downBlazing = new Question("Cancel-worthy", "S LP HZLUZLRXMYQD", "GO DOWN IN FLAMES", 39)
+const doneDead = new Question("Waiting to respawn", "UEMDQWEHQJYVQTEKDJ", "DOWN FOR THE COUNT", 17)
 
-const qs = [helloWorld]
+const qs = [helloWorld, goodLuck, catTongue, downBlazing, doneDead]
 const r = Math.floor(Math.random() * qs.length);
 
 document.getElementById("theme").innerHTML = qs[r].theme
 document.getElementById("cyphertext").innerHTML = qs[r].cyphertext
-document.getElementById("theme").innerHTML = qs[r].theme
+
