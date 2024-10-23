1b3ce64e4b4259f5066a3a97949b15ebaa493651
Glitch (glitch-hello-website) <none>
2024-10-23 11:49:52 +0000

    🐲🍱 Checkpoint ./script.js:37674724/1151 ./index.html:37674724/5484

--- script.js
+++ script.js
@@ -5,15 +5,43 @@ console.log("Hello 🌎");
 var keys = document.getElementById("keyTable");
 var table = document.createElement("table");
 
+const arr = [
+    ['A', 0],
+    ['B', 1],
+    ['C', 2],
+    ['D', 3],
+    ['E', 4],
+    ['F', 5],
+    ['G', 6],
+    ['H', 7],
+    ['I', 8],
+    ['J', 9],
+    ['K', 10],
+    ['L', 11],
+    ['M', 12],
+    ['N', 13],
+    ['O', 14],
+    ['P', 15],
+    ['Q', 16],
+    ['R', 17],
+    ['S', 18],
+    ['T', 19],
+    ['U', 20],
+    ['V', 21],
+    ['W', 22],
+    ['X', 23],
+    ['Y', 24],
+    ['Z', 25],
+    [' ', 26]
+  ]
+
 for (let i = 0; i < 2; i++) {
     const tr = table.insertRow();
-    for (let j = 0; j < 29; j++) {
-      const td = tr.insertCell();
-        td.appendChild(document.createTextNode(`Cell I${i}/J${j}`));
+    for (let j = 0; j < 28; j++) {
+        const td = tr.insertCell();
+        let txt = document.createTextNode(`tx`)
+        td.appendChild(txt);
         td.style.border = '1px solid black';
-        if (i === 1 && j === 1) {
-          td.setAttribute('rowSpan', '2');
-      }
     }
   }
   keys.appendChild(table);
@@ -91,6 +119,17 @@ function sp_hint() {
 
 function info() {
   document.getElementById("infoDialog").showModal();
+}
+
+function showKeys() {
+  var x = document.getElementById("keyTable");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    console.log("keys");
+  } else {
+    x.style.display = "none";
+    console.log("keys hide");
+  }
 }
 
 // CHECK ANS
