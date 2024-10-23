beb99053c91cc6f1fe810f3dd664b318fcbae690
Glitch (glitch-hello-website) <none>
2024-10-23 10:17:20 +0000

    🐁⛰ Checkpoint ./game.html:37674724/5421 ./script.js:37674724/3540

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ console.log("Hello 🌎");
 
 // RANDOMISE Q
 class Question {
-  constructor(t, c, a, k) {
+  constructor(t, c, a, k, h) {
     this.theme = t;
     this.cyphertext = c;
     this.answer = a;
     this.key = k;
+    this.hint = h;
   }
 }
 
@@ -15,26 +16,30 @@ const helloWorld = new Question(
   "Getting started...",
   "QNUUXIEX UM",
   "HELLO WORLD",
-  9
+  9,
+  "_____ ____D"
 );
-const goodLuck = new Question("Good luck!", "RGUQ PQPAUW", "BREAK A LEG", 16);
+const goodLuck = new Question("Good luck!", "RGUQ PQPAUW", "BREAK A LEG", 16, "_____ _ __G");
 const catTongue = new Question(
   "I'm speechless...",
   "ECVBIQVB QWTBVQPIWG",
   "CAT GOT YOUR TONGUE",
-  2
+  2,
+  "___ ___ ____ _____E"
 );
 const downBlazing = new Question(
   "Cancel-worthy",
   "S LP HZLUZLRXMYQD",
   "GO DOWN IN FLAMES",
-  39
+  39,
+  "__ ____ __ _____S"
 );
 const doneDead = new Question(
   "Waiting to respawn",
   "UEMDQWEHQJYVQTEKDJ",
   "DOWN FOR THE COUNT",
-  17
+  17,
+  "____ ___ ___ ____T"
 );
 
 const qs = [helloWorld, goodLuck, catTongue, downBlazing, doneDead];
@@ -42,6 +47,12 @@ const r = Math.floor(Math.random() * qs.length);
 
 document.getElementById("theme").innerHTML = qs[r].theme;
 document.getElementById("cyphertext").innerHTML = qs[r].cyphertext;
+document.getElementById("sp-hint").innerHTML = qs[r].cyphertext;
+
+// SHOW HINT
+function hint() {
+  document.getElementById("hintDialog").showModal();
+}
 
 // CHECK ANS
 let guesses = 0;
@@ -50,7 +61,7 @@ document.getElementById("guesses").innerHTML = guesses;
 function guess() {
   let ans = document.getElementById("answer").value;
   if (ans == qs[r].answer) {
-    document.getElementById("res").innerHTML = guesses+1;
+    document.getElementById("res").innerHTML = guesses + 1;
     document.getElementById("winDialog").showModal();
   } else {
     guesses += 1;
