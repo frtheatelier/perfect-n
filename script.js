dab4f9e8b3ada8cbf15168a530b216dc5958b3ec
Glitch (glitch-hello-website) <none>
2024-10-23 09:54:09 +0000

    ⛩🐉 Checkpoint ./script.js:37674724/7131 ./game.html:37674724/207

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 // Use your menus or right-click / control-click and choose "Inspect" > "Console"
 console.log("Hello 🌎");
 
-// BUTTON MOVEMENT (from template)
-const btn = document.querySelector("button");
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    btn.classList.toggle("dipped");
-  };
-}
-
 // RANDOMISE Q
 class Question {
   constructor(t, c, a, k) {
@@ -19,15 +11,49 @@ class Question {
   }
 }
 
-const helloWorld = new Question("Getting started...", "QNUUXIEX UM", "HELLO WORLD", 9)
-const goodLuck = new Question("Good luck!", "RGUQ PQPAUW", "BREAK A LEG", 16)
-const catTongue = new Question("I'm speechless...", "ECVBIQVB QWTBVQPIWG", "CAT GOT YOUR TONGUE", 2)
-const downBlazing = new Question("Cancel-worthy", "S LP HZLUZLRXMYQD", "GO DOWN IN FLAMES", 39)
-const doneDead = new Question("Waiting to respawn", "UEMDQWEHQJYVQTEKDJ", "DOWN FOR THE COUNT", 17)
+const helloWorld = new Question(
+  "Getting started...",
+  "QNUUXIEX UM",
+  "HELLO WORLD",
+  9
+);
+const goodLuck = new Question("Good luck!", "RGUQ PQPAUW", "BREAK A LEG", 16);
+const catTongue = new Question(
+  "I'm speechless...",
+  "ECVBIQVB QWTBVQPIWG",
+  "CAT GOT YOUR TONGUE",
+  2
+);
+const downBlazing = new Question(
+  "Cancel-worthy",
+  "S LP HZLUZLRXMYQD",
+  "GO DOWN IN FLAMES",
+  39
+);
+const doneDead = new Question(
+  "Waiting to respawn",
+  "UEMDQWEHQJYVQTEKDJ",
+  "DOWN FOR THE COUNT",
+  17
+);
 
-const qs = [helloWorld, goodLuck, catTongue, downBlazing, doneDead]
+const qs = [helloWorld, goodLuck, catTongue, downBlazing, doneDead];
 const r = Math.floor(Math.random() * qs.length);
 
-document.getElementById("theme").innerHTML = qs[r].theme
-document.getElementById("cyphertext").innerHTML = qs[r].cyphertext
+document.getElementById("theme").innerHTML = qs[r].theme;
+document.getElementById("cyphertext").innerHTML = qs[r].cyphertext;
 
+// CHECK ANS
+let guesses = 0;
+document.getElementById("guesses").innerHTML = guesses;
+
+function guess() {
+  let ans = document.getElementById("answer").innerHTML;
+  if (ans == qs[r].answer) {
+    document.getElementById("winDialog").showModal();
+  } else {
+    guesses += 1;
+    document.getElementById("guesses").innerHTML = guesses;
+    console.log("oops");
+  }
+}
