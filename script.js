e2334471686f403d7ee96ab0c3d70b4b57c8b9df
Glitch (glitch-hello-website) <none>
2024-10-25 21:32:23 +0000

    📅🐫 Checkpoint ./script.js:37674724/4103 ./index.html:37674724/3314

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
 // CHECK VALUE
-var n = document.getElementById("n-value").value;
+var n = document.getElementById("n-input").value;
 
 function find_factors(i) {
-    let factors = [1]
-    sqrt = Math.ceil(Math.sqrt(i))
-    for (let j = 0; j <= sqrt; j++) {
-      if (i % j == 0){
-        factors.append(j)
-      }
+  let factors = [1];
+  let root = Math.ceil(Math.sqrt(i));
+  for (let j = 0; j <= root; j++) {
+    if (i % j == 0 && !factors.includes(j)) {
+      console.log(j)
+      factors.append(j);
+      factors.append(i / j);
+      console.log("append", j)
     }
+  }
 
-    return factors
+  return factors;
 }
 
+function check_perfect(i) {
+  let f = find_factors(i);
+  let sum = 0;
+  f.forEach((number) => {
+    sum += number;
+  });
 
-function check_perfect(n: int) -> bool:
-    """check if n is perfect"""
-    factors = find_factors(n)
-    # print(factors)
-    return sum(factors) == n
+  return f, sum == i;
+}
+
+function analysis(i) {
+  if (check_perfect(i)){
+    return "A"
+  } else {
+    return "NOT A"
+  }
+}
 
 // SHOW INFO
 function showRes() {
+  document.getElementById("n-value").innerHTML = n;
+  document.getElementById("perf").innerHTML = analysis(n);
+  document.getElementById("factors").innerHTML = find_factors(n);
   document.getElementById("res").showModal();
 }
 
